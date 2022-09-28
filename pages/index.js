7b0d468341370f3be2b6d1fb6bb58ecdce9beadd
Robert <r.skral@apiko.com>
2022-09-28 13:04:41 +0300

    add trim

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,22 @@ import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const handleClick = () => {
-  const value = '1+2 + 4'
-  const operators = value.split(/[0-9]/).filter((item) => item !== '')
+  const value = '1+2 + 4 -1 '
+  const operators = value.split(/[0-9]/).filter((item) => item !== '').map((item) => item.trim())
   const num = value.split(/[+-]/).filter((item) => item !== '')
 
+
   const res = operators.reduce((memo, param, index) => {
-    // if (index <= num.length) {
-    if (param === '+') { return memo = memo + parseInt(num[index + 1]) }
-    if (param === '-') { return memo = memo - parseInt(num[index + 1]) }
-    // }?\
+    if (param === '+') {
+      return memo = memo + parseInt(num[index + 1])
+    }
+    if (param === '-') {
+      return memo = memo - parseInt(num[index + 1])
+    }
     return memo
   }, parseInt(num[0]))
-  console.log('==value==', operators)
-  console.log('==num==', num)
-  console.log('==res==', res)
 
+  console.log('==res==', res)
 }
 
 export default function Home() {
